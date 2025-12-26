5dd3b819b06d0c040d5cf248a2cc86577e6a3194
yunjioh <115560056+yunjioh@users.noreply.github.com>
2025-12-26 17:42:30 +0900

    test

--- js/index.js
+++ js/index.js
@@ -1,49 +1,4 @@
 
-// card lineup
-// document.addEventListener('DOMContentLoaded', () => {
-//   const wrap = document.querySelector('.card-wrap');
-//   const cards = wrap.querySelector('.cards');
-//   const card = cards.querySelector('.card');
-//   const next = wrap.querySelector('.next');
-//   const prev = wrap.querySelector('.prev');
-
-//   if (!wrap || !cards || !card || !next || !prev) return;
-
-//   let currentX = 0;
-
-//   const cardStyle = getComputedStyle(card);
-//   const gap = parseFloat(getComputedStyle(cards).columnGap || 0);
-//   const step = card.offsetWidth + gap;
-
-//   next.addEventListener('click', () => {
-//     const maxMove = cards.scrollWidth - wrap.clientWidth;
-//     currentX = Math.min(currentX + step, maxMove);
-//     cards.style.transform = `translateX(-${currentX}px)`;
-//   });
-
-//   prev.addEventListener('click', () => {
-//     currentX = Math.max(currentX - step, 0);
-//     cards.style.transform = `translateX(-${currentX}px)`;
-//   });
-
-//     // REMOVE 커서 UI 추가
-//   const cursor = document.querySelector('.cursor-ui');
-//   if (!cursor) return;
-
-//   wrap.addEventListener('mouseenter', () => {
-//     cursor.classList.add('is-active');
-//   });
-
-//   wrap.addEventListener('mouseleave', () => {
-//     cursor.classList.remove('is-active');
-//   });
-
-//   wrap.addEventListener('mousemove', (e) => {
-//     cursor.style.left = `${e.clientX}px`;
-//     cursor.style.top = `${e.clientY}px`;
-//   });
-
-// });
 
 //1m hover
 document.addEventListener('DOMContentLoaded', () => {
@@ -99,47 +54,6 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
-
-
-// //1m 호버
-// document.addEventListener('DOMContentLoaded', () => {
-//   const wrap = document.querySelector('.wrap-1m');
-//   const clean = wrap.querySelector('.clean');
-
-//   if (!wrap || !clean) return;
-
-//   wrap.addEventListener('mousemove', (e) => {
-//     const rect = wrap.getBoundingClientRect();
-//     const x = e.clientX - rect.left;
-//     const y = e.clientY - rect.top;
-
-//     const mask = `
-//       radial-gradient(
-//         circle 220px at ${x}px ${y}px,
-//         white 0%,
-//         white 55%,
-//         transparent 75%
-//       )
-//     `;
-
-//     clean.style.maskImage = mask;
-//     clean.style.webkitMaskImage = mask;
-//   });
-
-//   wrap.addEventListener('mouseleave', () => {
-//     const reset = `
-//       radial-gradient(
-//         circle 0px at 0px 0px,
-//         transparent 0%,
-//         transparent 100%
-//       )
-//     `;
-//     clean.style.maskImage = reset;
-//     clean.style.webkitMaskImage = reset;
-//   });
-// });
-
-
 // bottom-bookclass
 window.addEventListener("load", () => {
   gsap.registerPlugin(ScrollTrigger);
@@ -188,70 +102,6 @@ gsap.timeline({
   ScrollTrigger.refresh();
 });
 
-
-
-// // bottom bookclass circle
-// window.addEventListener("load", function () {
-//   gsap.registerPlugin(ScrollTrigger);
-
-//   gsap.set(".bottom-bookclass", {clipPath: "circle(50% at 50% 100%)"});
-
-//   gsap.timeline({
-//     scrollTrigger: {
-//       trigger: ".bottom-bookclass",
-//       start: "top 60%",
-//       end: "top 10%",
-//       scrub: 1,
-//       // markers: true,
-//       invalidateOnRefresh: true
-//     }
-//   })
-//   .to(".bottom-bookclass", {clipPath: "circle(150% at 50% 50%)", ease: "none", duration:10})
-
-//   ScrollTrigger.refresh();
-// });
-
-
-// //in card
-// window.addEventListener("load", () => {
-//   gsap.registerPlugin(ScrollTrigger);
-
-//   gsap.set(".lineup .cards .card", {
-//     autoAlpha: 0,
-//     x: 120
-//   });
-
-//   gsap.to(".lineup .cards .card", {
-//     autoAlpha: 1,
-//     x: 0,
-//     duration: 0.8,
-//     ease: "power3.out",
-//     stagger: 0.12,
-//     scrollTrigger: {
-//       trigger: ".lineup",
-//       start: "top 75%",
-//       end: "bottom 60%",
-//       toggleActions: "play none none none",
-//       scrub: 1,
-//     }
-//   });
-
-//   ScrollTrigger.refresh();
-// });
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   const wrap = document.querySelector('.card-wrap');
-//   if (!wrap) return;
-
-//   const cards = wrap.querySelector('.cards');
-//   const card = cards?.querySelector('.card');
-//   const next = wrap.querySelector('.next');
-//   const prev = wrap.querySelector('.prev');
-
-//   if (!cards || !card || !next || !prev) return;
-
-//   let currentX = 0;
-
 //   // flex gap 제대로 읽기
 //   const styles = getComputedStyle(cards);
 //   const gap = parseFloat(styles.gap || styles.columnGap || 0);
@@ -270,14 +120,6 @@ gsap.timeline({
 //     cards.style.transform = `translateX(-${currentX}px)`;
 //   });
 // });
-
-// $(function () {
-//   $('.animate').scrolla({
-//     mobile: true,
-//     once: false
-//   });
-// });
-
 
 document.addEventListener('DOMContentLoaded', () => {
   const wrap = document.querySelector('.card-wrap');
@@ -311,16 +153,6 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-
-
-
-
-
-
-
-
-
-
 $(function () {
   $('.animate').scrolla({
     mobile: true,
@@ -329,10 +161,9 @@ $(function () {
 });
 
 
-
 // scroll-in-ani
 document.addEventListener("DOMContentLoaded", () => {
-  const targets = document.querySelectorAll(".textAni-fast, .from-left, .from-right, .shake-top, .split, .puff-in-center");
+  const targets = document.querySelectorAll(".shake-top, .split, .puff-in-center");
 
   const observer = new IntersectionObserver(
     entries => {
@@ -359,27 +190,4 @@ document.addEventListener("DOMContentLoaded", () => {
 
 document.addEventListener("DOMContentLoaded", () => {
   Splitting({ target: ".split", by: "chars" });
-});
-
-const mm = gsap.matchMedia();
-
-mm.add("(min-width: 1399px)", () => {
-  gsap.from(".textAni-fast", {
-    y: 40,
-    opacity: 0,
-    duration: 1,
-    immediateRender: false,
-    scrollTrigger: {
-      trigger: ".textAni-fast",
-      start: "top 80%",
-    }
-  });
-});
-
-mm.add("(max-width: 768px)", () => {
-  gsap.set(".textAni-fast", {
-    opacity: 1,
-    transform: "none",
-    clearProps: "all",
-  });
 });
