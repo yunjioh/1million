afaa9efcb542e1f5e1321609bd5b3cb7421f75b9
yunjioh <dhdbswl08@naver.com>
2025-12-27 22:47:16 +0900

    test

--- sub-page/sub-schedule/js/schedule.js
+++ sub-page/sub-schedule/js/schedule.js
@@ -1,14 +1,10 @@
 
-        $(function() {
-	$('.animate').scrolla({
-		mobile: true, //모바일버전시 활성화
-		once: false //스크롤시 딱 한번만 하고싶을땐 true
-	});    
-        }); 
-
-
-
-
+$(function () {
+  $('.animate').scrolla({
+    mobile: true, //모바일버전시 활성화
+    once: false //스크롤시 딱 한번만 하고싶을땐 true
+  });
+});
 
 
 const overlay = document.querySelector('.classinformCard');
